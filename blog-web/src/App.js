d3b848a3ff360bef520985d912a057ded6eedcaf
Rahul Ghatge <rahulghatge166@gmail.com>
2025-02-26 21:17:52 +0530

    Added topbar

--- blog-web/src/App.js
+++ blog-web/src/App.js
@@ -1,50 +1,22 @@
-
 import './App.css';
-import { createBrowserRouter,createRoutesFromElements, RouterProvider, Route, Navigate } from "react-router-dom";
+import { 
+  createBrowserRouter, 
+  createRoutesFromElements, 
+  RouterProvider, 
+  Route, 
+  Navigate 
+} from "react-router-dom";
+import { useContext } from 'react';
+import { Context } from './Context/Context';
+
+// Components
 import Topbar from './Component/TopBar/Topbar';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
 import Register from './Pages/Register/Register';
 import Setting from './Pages/Setting/Setting';
-import Single from './Pages/Single/Single'
+import Single from './Pages/Single/Single';
 import Write from './Pages/Write/Write';
-import { useContext } from 'react';
-import { Context } from './Context/Context';
-
-
-
-
-// function App() {
-
-//   const {user} = useContext(Context);
-  
-//   const routers = createBrowserRouter(
-
-//     createRoutesFromElements(
-//       <Route path='/' element={<Register />}>
-//         <Route index element={<Home />} />
-//         <Route path='/register' element={<Register />} />
-//         <Route path='/setting' element={<Setting />} />
-//         <Route path='/write' element={<Write />} />
-//         {/* <Route>
-//         {user ? (<Route path='/write' element={<Write/>} />) : (<Route path='/register' element={<Register/>} />)}
-//         </Route> */}
-//         <Route path='/login' element={<Login />} />
-//         {/* {user ? (<Route index element={<Home/>}/>) : (<Route path='/login' element={<Login/>} />) } */}
-//         <Route path='/login'>
-//         {user ? <Route index element={<Home/>} /> : <Route path='/login' element={<Login/>} />}
-//         </Route>
-//         <Route path='/post/:id' element={<Single />} />
-//       </Route>
-//     )
-//   )
-  
-
-//   return (
-//     <RouterProvider router={routers} />
-//   );
-// }
-
 
 function App() {
   const { user } = useContext(Context);
@@ -52,13 +24,22 @@ function App() {
   const routers = createBrowserRouter(
     createRoutesFromElements(
       <>
-        {/* Redirect to Register if user is not logged in */}
-        <Route path="/" element={user ? <Home /> : <Navigate to="/register" />} />
+        <Route path="/" element={
+          <>
+            <Topbar />
+            {user ? <Home /> : <Navigate to="/register" />}
+          </>
+        } />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
         <Route path="/setting" element={user ? <Setting /> : <Navigate to="/login" />} />
         <Route path="/write" element={user ? <Write /> : <Navigate to="/login" />} />
-        <Route path="/post/:id" element={<Single />} />
+        <Route path="/post/:id" element={
+          <>
+            <Topbar />
+            <Single />
+          </>
+        } />
       </>
     )
   );
